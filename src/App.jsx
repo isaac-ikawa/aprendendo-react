be7a5198822c82170fef854f0d241cd58d96b7ce
IsaacIkawa <isaacikawa1@outlook.com>
2024-12-13 23:10:32 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Titulo from "./Titulo"
+
+function App(){
+  return (
+    <div>
+      <Titulo cor='red'/>
+      <Titulo cor='blue'/>
+      <Titulo cor='purple'/>
+      <Titulo cor='orangered'/>
+    </div>
+  ) 
+}
+
+export default App
